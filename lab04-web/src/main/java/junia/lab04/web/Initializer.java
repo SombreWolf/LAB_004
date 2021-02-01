5e49bec0ed51555e4c04abd849405d64d947f197
CharlesP-0 <56159744+CharlesP-0@users.noreply.github.com>
2021-02-01 16:02:15 +0100

    Ajout classe Initializer + implementations méthodes

---
+++ lab04-web/src/main/java/junia/lab04/web/Initializer.java
@@ -0,0 +1,29 @@
+package junia.lab04.web;
+
+import junia.lab04.core.config.AppConfig;
+import junia.lab04.core.config.DBConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class Initializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        AppConfig appConfig = new AppConfig();
+        DBConfig dbConfig = new DBConfig();
+        Class[] rootConfigClasses = new Class[2];
+        return rootConfigClasses;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        WebConfig webConfig = new WebConfig;
+        Class[] servletConfigClasses = new Class[1];
+        return servletConfigClasses;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        String[] servletMapping = new String[1];
+        servletMapping[0] = "/";
+        return servletMapping;
+    }
+}
