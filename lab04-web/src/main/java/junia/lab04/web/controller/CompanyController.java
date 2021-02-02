ff1e46e41b45b3fb556f0c0f34ac9d789b12a6b1
CharlesP-0 <56159744+CharlesP-0@users.noreply.github.com>
2021-02-02 08:45:52 +0100

    Ajout deuxième partie company controller (peut-être un petit bug à régler aussi)

--- lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/junia/lab04/web/controller/CompanyController.java
@@ -4,6 +4,7 @@ import junia.lab04.core.entity.Company;
 import junia.lab04.core.service.CompanyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,6 +24,18 @@ public class CompanyController {
         modelMap.addAttribute("companies",listOfCompanies);
         return "companiesList";
 
+    }
+    @RequestMapping("/form")
+    public String getForm(ModelMap modelMap){
+        Company company = new Company();
+        modelMap.addAttribute("company", company);
+        return "companyForm";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String submitForm (@ModelAttribute("company") Company company ){
+        companyService.save(company);
+        return "redirect:list";
     }
 
 }
